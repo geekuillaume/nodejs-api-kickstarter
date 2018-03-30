58cbf576bc3c8c8535208bdb9fea7106208ceb5c
Guillaume Besson <guillaume@besson.co>
2018-03-30 13:08:53 +0200

    Added a default local config

---
+++ config/local.js
@@ -0,0 +1,5 @@
+module.exports = {
+  // You can overwrite specific config variables here
+  // Because this file is in .gitignore, you will not accidentally commit it
+  // It's a good place to store secrets that should not be included in your git history
+};
